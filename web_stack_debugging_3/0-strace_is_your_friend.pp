7ddd0bb4f1b6de4371d26bea32b8b175798cb5d1
Isaiah-Essien <i.essien@alustudent.com>
2023-08-21 02:24:05 -0700

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 #puppet manifest to rename a file and create a file
+# class-wp-locale.php instead of class-wp-locale.phpp in wp-settings.php
 
-file {'/var/www/html/wp-includes/class-wp-locale.phpp':
-  ensure => present,
-    source => '/var/www/html/wp-includes/class-wp-locale.php',
-    }
+exec { 'fix file name':
+     command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+     provider => 'shell'
+}
